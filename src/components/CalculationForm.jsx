d03fa2489a1fa81440beeb46e7d9fdff215f675a
thetruespectre <abdullahasdaqarham@gmail.com>
2025-01-01 13:22:40 +0600

    Added CalculationHeader component

--- src/components/CalculationForm.jsx
+++ src/components/CalculationForm.jsx
@@ -3,6 +3,7 @@ import Tile from "./Tile";
 import { useForm } from "react-hook-form";
 import NumberField from "./NumberField";
 import { SubmitBtn } from "./SubmitBtn";
+import CalculationHeader from "./CalculationHeader";
 
 export const FormContext = createContext();
 
@@ -20,12 +21,7 @@ export function CalculationForm() {
 
   return (
     <>
-      <div className="mb-8">
-        <h1 className="font-bold text-5xl">Enter your Assets</h1>
-        <h3 className="mt-2 text-slate-700">
-          If a particular asset is not relevant to you, leave it empty
-        </h3>
-      </div>
+      <CalculationHeader />
 
       <FormContext.Provider
         value={{ register, handleSubmit, calculate, isSubmitSuccessful }}
---
+++ src/components/CalculationHeader.jsx
@@ -0,0 +1,10 @@
+export default function CalculationHeader() {
+  return (
+    <div className="mb-8">
+      <h1 className="font-bold text-5xl">Enter your Assets</h1>
+      <h3 className="mt-2 text-slate-700">
+        If a particular asset is not relevant to you, leave it empty
+      </h3>
+    </div>
+  );
+}
